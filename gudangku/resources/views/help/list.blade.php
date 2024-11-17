f16fe560aff66e8d8e8862bac0978e0a0263e524
Leonardho Rante S <newbie8801@gmail.com>
2024-11-17 15:19:40 +0700

    [FE] Some user manual
    See My Inventory or Report Calendar, Export Historyin CSV, Delete History, See My History, See and Toggle Stats View, Export All Inventory CSV, How to Sign Out?

--- gudangku/resources/views/help/list.blade.php
+++ gudangku/resources/views/help/list.blade.php
@@ -1,16 +1,30 @@
+<hr><h1 class="fw-bold my-3 text-center" style="font-size:calc(2*var(--textLG));">User Manual</h1>
 <div class="container bordered mb-4">
-    <h1 class="fw-bold mb-2" style="font-size:calc(2*var(--textLG));">How to Login?</h1>
+    <h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textLG));">How to Login?</h1>
     <h2 style="font-size:calc(var(--textLG));">To sign it to GudangKu Apps you must provide <b>Email / Username</b> and <b>Password</b></h2>
+    <h2 style="font-size:calc(var(--textLG));" class="fw-bold mt-3">How to do it :</h2>
     <ol class="text-white ms-4" style="list-style-type: circle;">
         <li>In the login page, find the login form that contain <b>Email / Username</b> and <b>Password</b></li>
         <li>Fill the form and Press the <b class="text-success">Submit</b> button to verify your login</li>
     </ol>
 </div>
-<h1 class="fw-bold mb-2" style="font-size:calc(2*var(--textXLG));">Inventory</h1>
-<div class="container bordered mb-3">
-    <h1 class="fw-bold mb-2" style="font-size:calc(2*var(--textLG));">How to Add Inventory?</h1>
+<div class="container bordered mb-4">
+    <h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textLG));">How to Sign Out?</h1>
+    <h2 style="font-size:calc(var(--textLG));">When sign out, you will also delete the stored local and session data of GudangKu</h2>
+    <h2 style="font-size:calc(var(--textLG));" class="fw-bold mt-3">How to do it :</h2>
+    <ol class="text-white ms-4" style="list-style-type: circle;">
+        <li>In the landing page, press the <b>My Profile</b> menu</li>
+        <li>It will takes you to My Profile Page, find and click the <b>Sign Out</b> button</li>
+        <li>A popup of recover confirmation will appear, click the button <b class="text-success">Yes, Sign Out</b> to process</li>
+    </ol>
+</div>
+
+<h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textXLG));">Inventory</h1>
+<div class="container bordered mb-4">
+    <h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textLG));">How to Add Inventory?</h1>
     <h2 style="font-size:calc(var(--textLG));">In this section you must provide <b>Pin Name</b>, <b>Category</b>, <b>Inventory Merk</b>, <b>Inventory Price</b>, <b>Inventory Volume</b>, <b>Inventory Unit</b>, and <b>Inventory Room</b></h2>
     <ol class="text-white ms-4" style="list-style-type: circle;">
+    <h2 style="font-size:calc(var(--textLG));" class="fw-bold mt-3">How to do it :</h2>
         <li>In the landing page, press the <b>My Inventory</b> menu</li>
         <li>It will takes you to My Inventory Page, find the <b>Add Inventory</b> button</li>
         <li>In the Add Inventory Page, fill the mandatory field first like the <b>Pin Name</b>, <b>Category</b>, <b>Inventory Merk</b>, <b>Inventory Price</b>, <b>Inventory Volume</b>, <b>Inventory Unit</b>, and <b>Inventory Room</b></li>
@@ -18,76 +32,157 @@
         <li>Press the <b class="text-success">Submit</b> button to finish your inventory add process</li>
     </ol>
 </div>
-<div class="container bordered mb-3">
-    <h1 class="fw-bold mb-2" style="font-size:calc(2*var(--textLG));">Browse your Inventory</h1>
+<div class="container bordered mb-4">
+    <h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textLG));">Browse your Inventory</h1>
     <h2 style="font-size:calc(var(--textLG));">In this section you will see all of your created inventory</h2>
+    <h2 style="font-size:calc(var(--textLG));" class="fw-bold mt-3">How to do it :</h2>
     <ol class="text-white ms-4" style="list-style-type: circle;">
         <li>In the landing page, press the <b>My Inventory</b> menu</li>
         <li>It will takes you to My Inventory Page, find a table at the <b>bottom of the section "Control Panel"</b></li>
-        <li>You <b class="text-success">will see</b> the <b>Inventory Name</b>, <b>Description</b>, <b>Category</b>, <b>Merk</b>, <b>Inventory Image</b>, <b>Favorite Status</b>, <b>Price</b>, <b>Unit & Capacity</b>, <b>Placement (Room, Storage, & Rack)</b>, <b>Reminder List</b>, and the <b>Properties</b></li>
+        <li>You <b class="text-success">will see</b> the <b>Inventory Name</b>, <b>Description</b>, <b>Category</b>, <b>Merk</b>, <b>Inventory Image</b>, <b>Favorite Status</b>, <b>Price</b>, <b>Unit & Capacity</b>, <b>Placement (Room, Storage, & Rack)</b>, <b>Reminder List</b>, and the <b>Properties</b>
+        . You can use the page nativator at the bottom of the page</li>
         <li>If you want to edit or see more detail. Click the button <b class="text-success fst-italic">with edit icon</b> and it will takes you to Edit Page</li>
     </ol>
 </div>
-<div class="container bordered mb-3">
-    <h1 class="fw-bold mb-2" style="font-size:calc(2*var(--textLG));">Search your Inventory using Name or Merk</h1>
+<div class="container bordered mb-4">
+    <h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textLG));">Search your Inventory using Name or Merk</h1>
     <h2 style="font-size:calc(var(--textLG));">In this section you will search inventory by providing the <b>Inventory Name</b> or <b>Inventory Merk</b></h2>
+    <h2 style="font-size:calc(var(--textLG));" class="fw-bold mt-3">How to do it :</h2>
     <ol class="text-white ms-4" style="list-style-type: circle;">
         <li>In the landing page, press the <b>My Inventory</b> menu</li>
         <li>It will takes you to My Inventory Page, find the section <b>"Control Panel"</b></li>
         <li>Type the <b>Inventory Name</b> or <b>Inventory Merk</b> in the field <b>Search by Name or Merk</b>. <b class='text-success'>Escape or press enter</b> to submit</li>
     </ol>
 </div>
-<div class="container bordered mb-3">
-    <h1 class="fw-bold mb-2" style="font-size:calc(2*var(--textLG));">Reset Inventory Search By Name or Merk</h1>
+<div class="container bordered mb-4">
+    <h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textLG));">Reset Inventory Search By Name or Merk</h1>
     <h2 style="font-size:calc(var(--textLG));">In this section you will reset the inventory search by <b>Name</b> or <b>Merk</b> and showing all the available inventory</h2>
+    <h2 style="font-size:calc(var(--textLG));" class="fw-bold mt-3">How to do it :</h2>
     <ol class="text-white ms-4" style="list-style-type: circle;">
         <li>In the landing page, press the <b>My Inventory</b> menu</li>
         <li>It will takes you to My Inventory Page, find the section <b>"Control Panel"</b></li>
         <li>In the field <b>Search by Name or Merk</b>. Press the button with <b class="text-success fst-italic">X icon</b> to reset the name or merk search</li>
     </ol>
 </div>
-<div class="container bordered mb-3">
-    <h1 class="fw-bold mb-2" style="font-size:calc(2*var(--textLG));">Search your Inventory using Category</h1>
+<div class="container bordered mb-4">
+    <h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textLG));">Search your Inventory using Category</h1>
     <h2 style="font-size:calc(var(--textLG));">In this section you will search inventory by providing the <b>Inventory Category</b></h2>
+    <h2 style="font-size:calc(var(--textLG));" class="fw-bold mt-3">How to do it :</h2>
     <ol class="text-white ms-4" style="list-style-type: circle;">
         <li>In the landing page, press the <b>My Inventory</b> menu</li>
         <li>It will takes you to My Inventory Page, find the section <b>"Control Panel"</b></li>
         <li>Select the <b>Search by Category</b> and it will <b class='text-success'>auto refresh</b> the list</li>
     </ol>
 </div>
-<div class="container bordered mb-3">
-    <h1 class="fw-bold mb-2" style="font-size:calc(2*var(--textLG));">Sorting your Inventory</h1>
+<div class="container bordered mb-4">
+    <h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textLG));">Sorting your Inventory</h1>
     <h2 style="font-size:calc(var(--textLG));">In this section you will sorting inventory list by some context</h2>
+    <h2 style="font-size:calc(var(--textLG));" class="fw-bold mt-3">How to do it :</h2>
     <ol class="text-white ms-4" style="list-style-type: circle;">
         <li>In the landing page, press the <b>My Inventory</b> menu</li>
         <li>It will takes you to My Inventory Page, find the section <b>"Control Panel"</b></li>
         <li>Select the <b>Sorting</b> and it will <b class='text-success'>auto refresh</b> the list</li>
     </ol>
 </div>
-<div class="container bordered mb-3">
-    <h1 class="fw-bold mb-2" style="font-size:calc(2*var(--textLG));">Delete Inventory</h1>
+<div class="container bordered mb-4">
+    <h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textLG));">Delete Inventory</h1>
     <h2 style="font-size:calc(var(--textLG));">In this section not permentally delete your inventory. You can recover the deleted item before 30 days after it got deleted</h2>
+    <h2 style="font-size:calc(var(--textLG));" class="fw-bold mt-3">How to do it :</h2>
     <ol class="text-white ms-4" style="list-style-type: circle;">
         <li>In the landing page, press the <b>My Inventory</b> menu</li>
         <li>It will takes you to My Inventory Page, choose the inventory you want to delete, and click the button <b class="fst-italic">with trash icon</b></li>
         <li>A popup of delete confirmation will appear, click the button <b class="text-success">Yes, Delete</b> to process</li>
     </ol>
 </div>
-<div class="container bordered mb-3">
-    <h1 class="fw-bold mb-2" style="font-size:calc(2*var(--textLG));">Recover Inventory</h1>
+<div class="container bordered mb-4">
+    <h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textLG));">Recover Inventory</h1>
     <h2 style="font-size:calc(var(--textLG));">In this section you can recover the deleted item before it passed 30 days</h2>
+    <h2 style="font-size:calc(var(--textLG));" class="fw-bold mt-3">How to do it :</h2>
     <ol class="text-white ms-4" style="list-style-type: circle;">
         <li>In the landing page, press the <b>My Inventory</b> menu</li>
         <li>It will takes you to My Inventory Page, choose the inventory you want to recover (the deleted item have section with red background), and click the button <b class="fst-italic">with rotate icon</b></li>
         <li>A popup of recover confirmation will appear, click the button <b class="text-success">Yes, Recover</b> to process</li>
     </ol>
 </div>
-<div class="container bordered">
-    <h1 class="fw-bold mb-2" style="font-size:calc(2*var(--textLG));">Permentally Delete Inventory</h1>
+<div class="container bordered mb-4">
+    <h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textLG));">Permentally Delete Inventory</h1>
     <h2 style="font-size:calc(var(--textLG));">In this section you will permentally delete your inventory, remove inventory from attached report, and reminder</h2>
+    <h2 style="font-size:calc(var(--textLG));" class="fw-bold mt-3">How to do it :</h2>
     <ol class="text-white ms-4" style="list-style-type: circle;">
         <li>In the landing page, press the <b>My Inventory</b> menu</li>
         <li>It will takes you to My Inventory Page, choose the inventory you want to permentally delete (the deleted item have section with red background), and click the button <b class="fst-italic">with fire icon</b></li>
         <li>A popup of recover confirmation will appear, click the button <b class="text-success">Yes, Delete</b> to process</li>
+    </ol>
+</div>
+<div class="container bordered mb-4">
+    <h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textLG));">Export All Inventory CSV</h1>
+    <h2 style="font-size:calc(var(--textLG));">In this section you will export the all inventory into CSV format and sended it to your Telegram Chat</h2>
+    <h2 style="font-size:calc(var(--textLG));" class="fw-bold mt-3">How to do it :</h2>
+    <ol class="text-white ms-4" style="list-style-type: circle;">
+        <li>In the landing page, press the <b>My Inventory</b> menu</li>
+        <li>It will takes you to My Inventory Page, at the top of the page click the <b class='text-success'>Save as CSV</b> button</li>
+    </ol>
+</div>
+
+<h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textXLG));">Stats</h1>
+<div class="container bordered mb-4">
+    <h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textLG));">See and Toggle Stats View</h1>
+    <h2 style="font-size:calc(var(--textLG));">In this section you can choose the chart type to show and the total view</h2>
+    <h2 style="font-size:calc(var(--textLG));" class="fw-bold mt-3">How to do it :</h2>
+    <ol class="text-white ms-4" style="list-style-type: circle;">
+        <li>In the landing page, press the <b>Stats</b> menu</li>
+        <li>It will takes you to My Stats Page, find the section <b>"Control Panel"</b></li>
+        <li>Select the <b>Chart Type</b> or <b>Toogle Total</b> to adjust the type of chart or value to present</li>
+        <li>If you want to get the CSV data, just click the <b>Download CSV</b> at the section you want to download</li>
+    </ol>
+    <h2 style="font-size:calc(var(--textLG));" class="fw-bold mt-3">Whats contain in it :</h2>
+    <ol class="text-white ms-4" style="list-style-type: circle;">
+        <li><b>Total Item Inventory By Category</b> present the data of total item or total price using pie chart, the data from all inventory separate by its top used category</li>
+        <li><b>Total Item Inventory By Room</b> present the data of total item or total price using pie chart, the data from all inventory separate by its top used room</li>
+        <li><b>Total Item Inventory By Favorite</b> present the data of total item or total price using pie chart, the data from all inventory separate by its like status</li>
+        <li><b>Total Item Inventory By Merk</b> present the data of total item or total price using pie chart, the data from all inventory separate by its top used merk</li>
+        <li><b>Total Inventory Created Per Month</b> present the data of total item and total price using line column chart, the data from total inventory created per month in a year</li>
+        <li><b>Total Report Created Per Month</b> present the data of total item and total price using line column chart, the data from total report created per month in a year</li>
+        <li><b>Total Report Spending Per Month</b> present the data of total item and total price using line column chart, the data from total report spending per month in a year</li>
+        <li><b>Total Report Used Per Month</b> present the data of total item and total price using line column chart, the data from total report used per month in a year</li>
+    </ol>
+</div>
+
+<h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textXLG));">History</h1>
+<div class="container bordered mb-4">
+    <h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textLG));">See My History</h1>
+    <h2 style="font-size:calc(var(--textLG));">In this section showing list of your activity in the Apps. That contain <b>Context</b> and <b>Datetime</b> when its happen</h2>
+    <ol class="text-white ms-4" style="list-style-type: circle;">
+        <li>In the landing page, press the <b>History</b> menu</li>
+        <li>It will takes you to My History Page, and you will see the <b class="text-success">list of activity</b>. You can use the page nativator at the bottom of the page</li>
+    </ol>
+</div>
+<div class="container bordered mb-4">
+    <h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textLG));">Delete History</h1>
+    <h2 style="font-size:calc(var(--textLG));">In this section, after delete the history. You can't recover it</h2>
+    <h2 style="font-size:calc(var(--textLG));" class="fw-bold mt-3">How to do it :</h2>
+    <ol class="text-white ms-4" style="list-style-type: circle;">
+        <li>In the landing page, press the <b>History</b> menu</li>
+        <li>It will takes you to My History Page, and you will see the list of activity. Choose the one you want to delete, and click the button <b class="fst-italic">with trash icon</b></li>
+        <li>A popup of delete confirmation will appear, click the button <b class="text-success">Yes, Delete</b> to process</li>
+    </ol>
+</div>
+<div class="container bordered mb-4">
+    <h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textLG));">Export Historyin CSV</h1>
+    <h2 style="font-size:calc(var(--textLG));">In this section you will export the history activity into CSV format and sended it to your Telegram Chat</h2>
+    <h2 style="font-size:calc(var(--textLG));" class="fw-bold mt-3">How to do it :</h2>
+    <ol class="text-white ms-4" style="list-style-type: circle;">
+        <li>In the landing page, press the <b>History</b> menu</li>
+        <li>It will takes you to History Page, at the top of the page click the <b class='text-success'>Save as CSV</b> button</li>
+    </ol>
+</div>
+
+<h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textXLG));">Calendar</h1>
+<div class="container bordered mb-4">
+    <h1 class="fw-bold mb-3" style="font-size:calc(2*var(--textLG));">See My Inventory or Report Calendar</h1>
+    <h2 style="font-size:calc(var(--textLG));">In this section showing the date when <b>inventory</b> or <b>report</b> are created in calendar format</h2>
+    <ol class="text-white ms-4" style="list-style-type: circle;">
+        <li>In the landing page, press the <b>Calendar</b> menu</li>
+        <li>It will takes you to Calendar Page, and you will see the <b class="text-success">calendar</b>. You can navigate using next and previous button at the top right of the calendar</li>
     </ol>
 </div>
