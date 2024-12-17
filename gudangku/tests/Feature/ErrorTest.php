9b084e70a880934a0784e6c930c00b074594841a
Leonardho Rante S <newbie8801@gmail.com>
2024-12-17 19:48:39 +0700

    [TEST] API Test for Get All Report, Get Reminder Mark, Post Re Remind

---
+++ gudangku/tests/Feature/ErrorTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use GuzzleHttp\Client;
+use Tests\TestCase;
+use App\Helpers\Audit;
+
+class ErrorTest extends TestCase
+{
+    protected $httpClient;
+    use LoginHelperTrait;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->httpClient = new Client([
+            'base_uri' => 'http://127.0.0.1:8000/api/v1/error/',
+            'http_errors' => false
+        ]);
+    }
+
+    public function test_get_all_error(): void
+    {
+        // Exec
+        $token = $this->login_trait("admin");
+        $response = $this->httpClient->get("", [
+            'headers' => [
+                'Authorization' => "Bearer $token"
+            ]
+        ]);
+
+        $data = json_decode($response->getBody(), true);
+
+        // Test Parameter
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertArrayHasKey('status', $data);
+        $this->assertEquals('success', $data['status']);
+        $this->assertArrayHasKey('message', $data);
+        $this->assertArrayHasKey('data', $data);
+
+        foreach ($data['data']['data'] as $dt) {
+            $check_object = ['id','message','stack_trace','file','line','is_fixed','created_at','faced_by'];
+            foreach ($check_object as $col) {
+                $this->assertArrayHasKey($col, $dt);
+            }
+
+            $check_not_null_str = ['message','stack_trace','file','created_at'];
+            foreach ($check_not_null_str as $col) {
+                $this->assertNotNull($dt[$col]);
+                $this->assertIsString($dt[$col]);
+            }
+
+            if (!is_null($dt['faced_by'])) {
+                $this->assertIsString($dt['faced_by']);
+                $this->assertEquals(36,strlen($dt['faced_by']));
+            }
+
+            $check_not_null_int = ['id','line','is_fixed'];
+            foreach ($check_not_null_int as $col) {
+                $this->assertNotNull($dt[$col]);
+                $this->assertIsInt($dt[$col]);
+                $this->assertGreaterThanOrEqual(0, $dt[$col]);
+            }
+
+            $this->assertContains($dt['is_fixed'], [0, 1]);
+        }
+
+        Audit::auditRecordText("Test - Get All Error", "TC-XXX", "Result : ".json_encode($data));
+        Audit::auditRecordSheet("Test - Get All Error", "TC-XXX", 'TC-XXX test_get_all_error', json_encode($data));
+    }
+}
--- gudangku/tests/Feature/ReminderTest.php
+++ gudangku/tests/Feature/ReminderTest.php
@@ -76,4 +76,67 @@ class ReminderTest extends TestCase
         Audit::auditRecordText("Test - Delete Reminder By Id", "TC-XXX", "Result : ".json_encode($data));
         Audit::auditRecordSheet("Test - Delete Reminder By Id", "TC-XXX", 'TC-XXX test_delete_reminder_by_id', json_encode($data));
     }
+
+    public function test_get_reminder_mark(): void
+    {
+        // Exec
+        $token = $this->login_trait("admin");
+        $response = $this->httpClient->get("mark", [
+            'headers' => [
+                'Authorization' => "Bearer $token"
+            ]
+        ]);
+
+        $data = json_decode($response->getBody(), true);
+
+        // Test Parameter
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertArrayHasKey('status', $data);
+        $this->assertEquals('success', $data['status']);
+        $this->assertArrayHasKey('message', $data);
+        $this->assertArrayHasKey('data', $data);
+
+        foreach ($data['data']['data'] as $dt) {
+            $check_object = ['inventory_name','inventory_category','reminder_desc','reminder_type','reminder_context','last_execute','created_at','username'];
+            foreach ($check_object as $col) {
+                $this->assertArrayHasKey($col, $dt);
+            }
+
+            $check_not_null_str = ['inventory_name','inventory_category','reminder_desc','reminder_type','reminder_context','last_execute','created_at','username'];
+            foreach ($check_not_null_str as $col) {
+                $this->assertNotNull($dt[$col]);
+                $this->assertIsString($dt[$col]);
+            }
+        }
+
+        Audit::auditRecordText("Test - Get Reminder Mark", "TC-XXX", "Result : ".json_encode($data));
+        Audit::auditRecordSheet("Test - Get Reminder Mark", "TC-XXX", 'TC-XXX test_get_reminder_mark', json_encode($data));
+    }
+
+    public function test_post_re_remind(): void
+    {
+        // Exec
+        $token = $this->login_trait("admin");
+        $body = [
+            "reminder_id" => "d6260a32-f612-11ee-b7be-3216422910e7"
+        ];
+        $response = $this->httpClient->post("re_remind", [
+            'headers' => [
+                'Authorization' => "Bearer $token"
+            ],
+            'json' => $body
+        ]);
+
+        $data = json_decode($response->getBody(), true);
+
+        // Test Parameter
+        $this->assertEquals(201, $response->getStatusCode());
+        $this->assertArrayHasKey('status', $data);
+        $this->assertEquals('success', $data['status']);
+        $this->assertArrayHasKey('message', $data);
+        $this->assertEquals('reminder re-executed', $data['message']);
+
+        Audit::auditRecordText("Test - Post Re-Remind", "TC-XXX", "Result : ".json_encode($data));
+        Audit::auditRecordSheet("Test - Post Re-Remind", "TC-XXX", 'TC-XXX test_post_re_remind', json_encode($data));
+    }
 }
